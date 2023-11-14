e9ee18d71a4afbc78ec1229a09d03c35e3c8e570
PhillipTwenk <118554161+PhillipTwenk@users.noreply.github.com>
2023-11-14 20:32:47 +0300

    Start working on movement
    -Add First movement controller
    -Add Camera controller

---
+++ NTOBearGame/Assets/Scripts/ForMovements/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform transformPlayer;
+    public Vector3 offset;
+    [SerializeField]private float speedChangePositionCam;
+    void Start()
+    {
+        speedChangePositionCam = 5f;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newCamPosition = new Vector3(offset.x, transformPlayer.position.y + offset.y, transformPlayer.position.z + offset.z);
+        transform.position = Vector3.Lerp(transform.position, newCamPosition, speedChangePositionCam * Time.deltaTime);
+    }
+}
