049db5ed14c00d4f19c2b312ffc13a9b9a3877e8
PhillipTwenk <118554161+PhillipTwenk@users.noreply.github.com>
2023-12-08 01:38:19 +0300

    Quest System 9

--- NTOBearGame/Assets/Scripts/Quest Scripts/ChatSystem.cs
+++ NTOBearGame/Assets/Scripts/Quest Scripts/ChatSystem.cs
@@ -2,35 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class ChatSystem : MonoBehaviour
 {
-    public GameObject chatPanel, textObject;
+    public GameObject chatPanel, textObjectPrefab;
     List<Message> messageList = new List<Message>();
+    public RectTransform newObjectTransform;
+    public Transform AllMessages;
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            GetMessage("pqpqpq", textObjectPrefab);
+        }
     }
-    public void GetMessage(string text) {
+    public void GetMessage(string text, GameObject textObject) {
 
         Message newMessage = new Message();
 
         newMessage.text = text;
 
-        GameObject newText = Instantiate(textObject, chatPanel.transform);
+        GameObject newText = Instantiate(textObject, newObjectTransform);
 
-        newMessage.textObject = newText.GetComponent<Text>();
+        newText.transform.parent = AllMessages;
 
-        newMessage.textObject.text = newMessage.text;
+        Debug.Log(newObjectTransform.localPosition);
+
+        newObjectTransform.localPosition = new Vector3(newObjectTransform.localPosition.x, newObjectTransform.localPosition.y - 120, newObjectTransform.localPosition.z);
+        Debug.Log(newObjectTransform.localPosition);
+        newMessage.textObjectClass = newText.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        
+        newMessage.textObjectClass.text = newMessage.text;
+
         messageList.Add(newMessage);
     }
 }
 public class Message
 {
     public string text;
-    public Text textObject;
+    public TextMeshProUGUI textObjectClass;
 }
